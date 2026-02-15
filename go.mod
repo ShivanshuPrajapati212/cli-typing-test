fbf113b774f8b9835e503b82cda66ec0bd42757d
Shivanshu Prajapati <shivanshuprajapati212@gmail.com>
2026-02-15 12:57:07 +0530

    text centered

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/ShivanshuPrajapati/cli-typing-test
 
 go 1.25.4
+
+require (
+	golang.org/x/sys v0.41.0 // indirect
+	golang.org/x/term v0.40.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,4 +1,32 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"golang.org/x/term"
+)
+
+var quote string = "hello bro you are nice man, and i like you to be my friend"
+
 func main() {
+	width, height, err := term.GetSize(int(os.Stdout.Fd()))
+	if err != nil {
+		log.Fatalf("Could not get terminal size: %v", err)
+	}
+
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	fmt.Print("\033[2J")
+
+	fmt.Printf("\033[%d;%dH", height/2, (width/2)-len(quote)/2)
+
+	fmt.Print(quote)
+
+	sig := <-sigs
+	fmt.Print(sig)
 }
